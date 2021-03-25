3723a73fc3f8efa7fa76b2ecb80daaaed569279f
MamoruTFN <paweenwich.t@ku.th>
2021-03-25 17:38:19 +0700

    cube free

--- 6320502461_quiz2_1.c
+++ 6320502461_quiz2_1.c
@@ -2,14 +2,39 @@
 
 int main()
 {
-    int n;
+    int n,count=0,i,m,c=0;
     scanf("%d",&n);
-    for(int i=1;i<=n;i++)
+    m=n;
+    for(i=2; i<=n; i++)
     {
-        if(n%i==0)
+        for(int j=0; j<3; j++)
         {
+            if(m%i==0)
+            {
+                m/=i;
+                count++;
+            }
 
         }
+        if(count==3&&m==1)
+        {
+            break;
+        }
+        else
+        {
+            count=0;
+            m=n;
+            c++;
+        }
+    }
+
+    if(count==3)
+    {
+        printf("Not Cube Free");
+    }
+    else
+    {
+        printf("%d",c);
     }
 
     return 0;
